58a59532b553368a04d79ad135f863bbebe574a8
arthur-a-ribeiro <pedrohdkovalczuk@gmail.com>
2024-09-11 17:02:34 -0300

    File input and Section Division

--- src/components/MetadataEditor.tsx
+++ src/components/MetadataEditor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Grid, TextField, MenuItem, Select, FormControl, InputLabel, Typography } from '@mui/material';
+import { Box, Grid, TextField, MenuItem, Select, FormControl, InputLabel, Typography, Divider } from '@mui/material';
 import { MetadataTypeForm, MetadataField } from '../interfaces/app_interfaces';
+import metadataGroups from '../assets/metadata_section_divider.json';
 
 interface MetametadataComponentProps {
   metadata: MetadataTypeForm | null;
@@ -25,128 +26,138 @@ const MetadataEditor: React.FC<MetametadataComponentProps> = ({ metadata, setMet
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        {metadata?.metadata_fields.map((field) => (
-          <Grid item xs={12} sm={4} key={field.id}>
-            {field.possible_values ? (
-              <FormControl
-                fullWidth
-                variant="outlined"
-                disabled={field.is_static}
-                sx={{
-                  backgroundColor: field.default_value ? 'grey.200' : 'inherit',
-                }}
-              >
-                <InputLabel
-                  sx={{
-                    fontFamily: 'Nunito',
-                    '&.Mui-focused': {
-                      color: 'green',
-                    },
-                  }}
-                >
-                  {field.label}
-                </InputLabel>
-                <Select
-                  value={getDropdownValue(field)}
-                  onChange={(e) => handleFieldChange(field.id, e.target.value as string)}
-                  label={field.label}
-                  sx={{
-                    fontFamily: 'Nunito',
-                    '& .MuiOutlinedInput-root': {
-                      '& fieldset': {
-                        borderColor: 'grey',
-                      },
-                      '&:hover fieldset': {
-                        borderColor: 'grey',
-                      },
-                      '&.Mui-focused fieldset': {
-                        borderColor: 'green',
-                      },
-                    },
-                  }}
-                >
-                  {field.possible_values.split(',').map((option: string) => (
-                    <MenuItem key={option.trim()} value={option.trim()}>
-                      {option.trim()}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            ) : field.field_type === 'string' || field.field_type === 'int' || field.field_type === 'float' ? (
-              <TextField
-                label={field.label}
-                variant="outlined"
-                fullWidth
-                value={field.default_value}
-                onChange={(e) => handleFieldChange(field.id, e.target.value)}
-                disabled={field.is_static}
-                InputLabelProps={{
-                  sx: {
-                    fontFamily: 'Nunito',
-                    '&.Mui-focused': {
-                      color: 'green',
-                    },
-                  },
-                }}
-                sx={{
-                  backgroundColor: field.default_value ? 'grey.200' : 'inherit',
-                  fontFamily: 'Nunito',
-                  '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                      borderColor: 'grey',
-                    },
-                    '&:hover fieldset': {
-                      borderColor: 'grey',
-                    },
-                    '&.Mui-focused fieldset': {
-                      borderColor: 'green',
-                    },
-                  },
-                }}
-              />
-            ) : field.field_type === 'date' ? (
-              <TextField
-                label={field.label}
-                type="date"
-                variant="outlined"
-                fullWidth
-                InputLabelProps={{
-                  shrink: true,
-                  sx: {
-                    fontFamily: 'Nunito',
-                    '&.Mui-focused': {
-                      color: 'green',
-                    },
-                  },
-                }}
-                value={field.default_value}
-                onChange={(e) => handleFieldChange(field.id, e.target.value)}
-                disabled={field.is_static}
-                sx={{
-                  fontFamily: 'Nunito',
-                  backgroundColor: field.default_value ? 'grey.200' : 'inherit',
-                  '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                      borderColor: 'grey',
-                    },
-                    '&:hover fieldset': {
-                      borderColor: 'grey',
-                    },
-                    '&.Mui-focused fieldset': {
-                      borderColor: 'green',
-                    },
-                  },
-                }}
-              />
-            ) : (
-              <Typography variant="body2" color="error">
-                Unsupported field type: {field.field_type}
-              </Typography>
-            )}
+      {metadataGroups.map((group) => (
+        <Box key={group.id} sx={{ mb: 4 }}>
+          <Divider sx={{ borderWidth: 1, borderColor: 'green', mb: 2, mt: group.name === "Metametadados" ? 2 : 8 }} />
+          <Typography variant="h5" sx={{ mb: 4, fontFamily: 'Nunito', color: "green", fontWeight: 'bold'}}>
+            {group.name}
+          </Typography>
+          <Grid container spacing={2}>
+            {metadata?.metadata_fields
+              .filter(field => group.values.includes(field.iso_xml_path))
+              .map((field) => (
+                <Grid item xs={12} sm={4} key={field.id}>
+                  {field.possible_values ? (
+                    <FormControl
+                      fullWidth
+                      variant="outlined"
+                      disabled={field.is_static}
+                      sx={{
+                        backgroundColor: field.default_value ? 'grey.200' : 'inherit',
+                      }}
+                    >
+                      <InputLabel
+                        sx={{
+                          fontFamily: 'Nunito',
+                          '&.Mui-focused': {
+                            color: 'green',
+                          },
+                        }}
+                      >
+                        {field.label}
+                      </InputLabel>
+                      <Select
+                        value={getDropdownValue(field)}
+                        onChange={(e) => handleFieldChange(field.id, e.target.value as string)}
+                        label={field.label}
+                        sx={{
+                          fontFamily: 'Nunito',
+                          '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                              borderColor: 'grey',
+                            },
+                            '&:hover fieldset': {
+                              borderColor: 'grey',
+                            },
+                            '&.Mui-focused fieldset': {
+                              borderColor: 'green',
+                            },
+                          },
+                        }}
+                      >
+                        {field.possible_values.split(',').map((option: string) => (
+                          <MenuItem key={option.trim()} value={option.trim()}>
+                            {option.trim()}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  ) : field.field_type === 'string' || field.field_type === 'int' || field.field_type === 'float' ? (
+                    <TextField
+                      label={field.label}
+                      variant="outlined"
+                      fullWidth
+                      value={field.default_value}
+                      onChange={(e) => handleFieldChange(field.id, e.target.value)}
+                      disabled={field.is_static}
+                      InputLabelProps={{
+                        sx: {
+                          fontFamily: 'Nunito',
+                          '&.Mui-focused': {
+                            color: 'green',
+                          },
+                        },
+                      }}
+                      sx={{
+                        backgroundColor: field.default_value ? 'grey.200' : 'inherit',
+                        fontFamily: 'Nunito',
+                        '& .MuiOutlinedInput-root': {
+                          '& fieldset': {
+                            borderColor: 'grey',
+                          },
+                          '&:hover fieldset': {
+                            borderColor: 'grey',
+                          },
+                          '&.Mui-focused fieldset': {
+                            borderColor: 'green',
+                          },
+                        },
+                      }}
+                    />
+                  ) : field.field_type === 'date' ? (
+                    <TextField
+                      label={field.label}
+                      type="date"
+                      variant="outlined"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                        sx: {
+                          fontFamily: 'Nunito',
+                          '&.Mui-focused': {
+                            color: 'green',
+                          },
+                        },
+                      }}
+                      value={field.default_value}
+                      onChange={(e) => handleFieldChange(field.id, e.target.value)}
+                      disabled={field.is_static}
+                      sx={{
+                        fontFamily: 'Nunito',
+                        backgroundColor: field.default_value ? 'grey.200' : 'inherit',
+                        '& .MuiOutlinedInput-root': {
+                          '& fieldset': {
+                            borderColor: 'grey',
+                          },
+                          '&:hover fieldset': {
+                            borderColor: 'grey',
+                          },
+                          '&.Mui-focused fieldset': {
+                            borderColor: 'green',
+                          },
+                        },
+                      }}
+                    />
+                  ) : (
+                    <Typography variant="body2" color="error">
+                      Unsupported field type: {field.field_type}
+                    </Typography>
+                  )}
+                </Grid>
+              ))}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      ))}
     </Box>
   );
 };
--- src/components/ProductUpload.tsx
+++ src/components/ProductUpload.tsx
@@ -1,124 +1,193 @@
-import React, { useState } from 'react';
+import AddIcon from "@mui/icons-material/Add";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import PanToolAlt from "@mui/icons-material/PanToolAlt";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
-import PanToolAlt from "@mui/icons-material/PanToolAlt";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import Select from "@mui/material/Select";
+import CircularProgress from "@mui/material/CircularProgress";
 import MenuItem from "@mui/material/MenuItem";
-import { Typography } from '@mui/material';
-import { SelectChangeEvent } from "@mui/material/Select"; 
-import { MetadataTypeForm, UploadJSON } from '../interfaces/app_interfaces';
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import React, { useRef, useState } from "react";
+import { MetadataTypeForm, UploadJSON } from "../interfaces/app_interfaces";
 
-import upload_data from '../assets/upload_response_example.json';
-import product_data from '../assets/product_type_form-response.json';
+import product_data from "../assets/product_type_form-response.json";
+import upload_data from "../assets/upload_response_example.json";
 
 interface ProductUploadProps {
-	setDisabledItems: (items: boolean[]) => void;
-	setProductName: (items: string) => void;
-	isLoadedProduct: boolean;
-	setLoadedProduct: (items: boolean) => void;
-	metadata: MetadataTypeForm | null;
-	setMetadata: React.Dispatch<React.SetStateAction<MetadataTypeForm | null>>;
-	setSelectedItem: (item: string) => void;
+  setDisabledItems: (items: boolean[]) => void;
+  setProductName: (items: string) => void;
+  isLoadedProduct: boolean;
+  setLoadedProduct: (items: boolean) => void;
+  metadata: MetadataTypeForm | null;
+  setMetadata: React.Dispatch<React.SetStateAction<MetadataTypeForm | null>>;
+  setSelectedItem: (item: string) => void;
 }
 
-const ProductUpload: React.FC<ProductUploadProps> = ({ 
-	setDisabledItems, 
-	setProductName, 
-	isLoadedProduct, 
-	setLoadedProduct,
-	metadata,
-	setMetadata,
-	setSelectedItem
+const ProductUpload: React.FC<ProductUploadProps> = ({
+  setDisabledItems,
+  setProductName,
+  isLoadedProduct,
+  setLoadedProduct,
+  metadata,
+  setMetadata,
+  setSelectedItem,
 }) => {
-	const [productID, setProductID] = useState<number | "">(""); // Ensure productID can only be a number or empty string
+  const [productID, setProductID] = useState<number | "">("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState<boolean>(false); // New loading state
 
-	const handleUpload = () => {
-		const productName = upload_data.serializer_data.geodata_file;
-		const parts = productName.split('/');
-		setProductName(parts[parts.length - 1]);
-		setLoadedProduct(true);
-	};
+  // Use a ref to programmatically click the file input
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-	const updateMetadataFields = (metadata: MetadataTypeForm, jsonData: UploadJSON): MetadataTypeForm => {
-		const updatedFields = metadata.metadata_fields.map(field => {
-			const jsonValue = jsonData.metadata[field.iso_xml_path];
-			if (jsonValue !== undefined) {
-				return { ...field, default_value: jsonValue.toString().trim() };
-			}
-			return field;
-		});
-		return { ...metadata, metadata_fields: updatedFields };
-	};
+  // Handle file selection
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
 
-	const handleChange = (event: SelectChangeEvent<number>) => {
-		const selectedID = event.target.value as number; // Ensure selectedID is a number
-		setProductID(selectedID);
-		const selectedMetadata = product_data.find(item => item.id === selectedID) || null;
-		if (selectedMetadata) {
-			const updatedMetadata = updateMetadataFields(selectedMetadata, upload_data);
-			setMetadata(updatedMetadata);
-			setDisabledItems([false, false, false, false, false, false]);
-			console.log(updatedMetadata);
-		}
-	};
+      // Set loading state to true while processing the file
+      setLoading(true);
 
-	return (
-		<Box sx={{ flexGrow: 1, mt: 4 }}>
-			<Button
-				variant="contained"
-				color="success"
-				startIcon={<AddIcon />}
-				sx={{ marginRight: 2, fontFamily: 'Nunito', fontWeight: 'bold' }}
-				onClick={handleUpload}
-			>
-				Adicionar novo
-			</Button>
-			<Button
-				variant="contained"
-				color="success"
-				startIcon={<PanToolAlt />}
-				sx={{ fontFamily: 'Nunito', fontWeight: 'bold' }}
-			>
-				Selecionar produto
-			</Button>
-			{isLoadedProduct && (
-				<Box sx={{ mt: 4, width: 500, mb: 6 }}>
-					<Typography style={{ marginBottom: 5, marginLeft: 5, fontFamily: 'Nunito' }}>
-						Selecione o tipo do produto carregado acima (Obrigatório)
-					</Typography>
-					<Select
-						value={productID}
-						onChange={handleChange}
-						displayEmpty
-						fullWidth
-						sx={{ fontFamily: 'Nunito' }}
-					>
-						<MenuItem value="" disabled>
-							Tipos disponíveis
-						</MenuItem>
-						{product_data.map((item) => (
-							<MenuItem key={item.id} value={item.id}>
-								{item.name}
-							</MenuItem>
-						))}
-					</Select>
-				</Box>
-			)}
-			{metadata && (
-				<Button
-					sx={{ justifyContent: "flex-end", marginLeft: 34, fontWeight: 'bold', fontFamily: 'Nunito' }}
-					variant="text"
-					color="success"
-					endIcon={<ArrowForwardIcon />}
-					onClick={() => setSelectedItem("Editar Metadados")}
-				>
-					Preencher Metadados
-				</Button>
-			)}
-		</Box>
-	);
+      // Simulate file loading delay or large file processing
+      try {
+        // Here you can add any asynchronous logic for processing the file
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay (2 seconds)
+        setSelectedFile(file);
+        const productName = file.name;
+        setProductName(productName);
+        setLoadedProduct(true);
+      } catch (error) {
+        console.error("File processing error: ", error);
+      } finally {
+        // Turn off the loading state once the file is processed
+        setLoading(false);
+      }
+    }
+  };
+
+  // Trigger the file input click programmatically when clicking the "Adicionar novo" button
+  const handleUploadClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click(); // Programmatically open the file explorer
+    }
+  };
+
+  const updateMetadataFields = (
+    metadata: MetadataTypeForm,
+    jsonData: UploadJSON
+  ): MetadataTypeForm => {
+    const updatedFields = metadata.metadata_fields.map((field) => {
+      const jsonValue = jsonData.metadata[field.iso_xml_path];
+      if (jsonValue !== undefined) {
+        return { ...field, default_value: jsonValue.toString().trim() };
+      }
+      return field;
+    });
+    return { ...metadata, metadata_fields: updatedFields };
+  };
+
+  const handleChange = (event: SelectChangeEvent<number>) => {
+    const selectedID = event.target.value as number; // Ensure selectedID is a number
+    setProductID(selectedID);
+    const selectedMetadata =
+      product_data.find((item) => item.id === selectedID) || null;
+    if (selectedMetadata) {
+      const updatedMetadata = updateMetadataFields(
+        selectedMetadata,
+        upload_data
+      );
+      setMetadata(updatedMetadata);
+      setDisabledItems([false, false, false, false, false, false]);
+      console.log(updatedMetadata);
+    }
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, mt: 4 }}>
+      {/* Hidden file input field */}
+      <input
+        type="file"
+        accept="*" // Accept any type of file
+        style={{ display: "none" }} // Hide the file input
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
+
+      {/* Show loading spinner while file is being processed */}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Button
+            variant="contained"
+            color="success"
+            startIcon={<AddIcon />}
+            sx={{ marginRight: 2, fontFamily: "Nunito", fontWeight: "bold" }}
+            onClick={handleUploadClick} // Trigger the file selection when clicked
+            disabled={loading} // Disable button during loading
+          >
+            Adicionar novo
+          </Button>
+
+          <Button
+            variant="contained"
+            color="success"
+            startIcon={<PanToolAlt />}
+            sx={{ fontFamily: "Nunito", fontWeight: "bold" }}
+            disabled={loading} // Disable button during loading
+          >
+            Selecionar produto
+          </Button>
+
+          {isLoadedProduct && (
+            <Box sx={{ mt: 4, width: 500, mb: 6 }}>
+              <Typography
+                style={{ marginBottom: 5, marginLeft: 5, fontFamily: "Nunito" }}
+              >
+                Selecione o tipo do produto carregado acima (Obrigatório)
+              </Typography>
+              <Select
+                value={productID}
+                onChange={handleChange}
+                displayEmpty
+                fullWidth
+                sx={{ fontFamily: "Nunito" }}
+                disabled={loading} // Disable dropdown during loading
+              >
+                <MenuItem value="" disabled>
+                  Tipos disponíveis
+                </MenuItem>
+                {product_data.map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Box>
+          )}
+
+          {metadata && (
+            <Button
+              sx={{
+                justifyContent: "flex-end",
+                marginLeft: 34,
+                fontWeight: "bold",
+                fontFamily: "Nunito",
+              }}
+              variant="text"
+              color="success"
+              endIcon={<ArrowForwardIcon />}
+              onClick={() => setSelectedItem("Editar Metadados")}
+              disabled={loading} // Disable button during loading
+            >
+              Preencher Metadados
+            </Button>
+          )}
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default ProductUpload;
